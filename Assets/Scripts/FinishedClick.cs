2d8978d7e781642d6bb6bed3a1fd2916844da862
EZeng1259 <elz13@scarletmail.rutgers.edu>
2024-05-16 23:58:00 -0400

    changes

--- Assets/Scripts/FinishedClick.cs
+++ Assets/Scripts/FinishedClick.cs
@@ -1,39 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Networking;
 
-public class FinishedClick : MonoBehaviour {
-	[SerializeField] GameObject[] Objects;
+public class FinishedClick : MonoBehaviour
+{
+    [SerializeField] GameObject[] Objects;
     public Button button;
+
+    public string apiUrl = "https://salty-thicket-48002.herokuapp.com/write_data"; // API URL
 
     private void Start()
     {
-        button.onClick.AddListener(RecordLocations);
+        button.gameObject.SetActive(false);
     }
 
-    public void RecordLocations(){
-    	var csv = new StringBuilder();
-        var columnNames = "ParticipantID,Store,xPos,yPos";
-        string[] objNames;
-        objNames = new string[19]{ "Gym", "Hardware Store", "Music Store", "Pharmacy",  "Bakery", "Bank", "Dentist", "Cafe", "Jewelry", "Butcher", "Supermarket",
-                "Bike Shop", "Pizzeria", "Toy Store", "Book Store", "Barber", "Boutique", "Gallery", "Pet Store"};
-        csv.AppendLine(columnNames);
+    private void Update()
+    {
+        if (DragandDrop.alreadyDragged.Count == 19)
+        {
+            button.gameObject.SetActive(true);
+            button.onClick.AddListener(RecordLocations);
+        }
+    }
+
+    public void RecordLocations()
+    {
+        string[] objNames = new string[19]
+        {
+            "Gym", "Hardware Store", "Music Store", "Pharmacy",  "Bakery", "Bank", "Dentist", "Cafe",
+            "Jewelry", "Butcher", "Supermarket", "Bike Shop", "Pizzeria", "Toy Store", "Book Store",
+            "Barber", "Boutique", "Gallery", "Pet Store"
+        };
+
         for (int i = 0; i < Objects.Length; i++)
         {
-            var xPos = Objects[i].transform.position.x.ToString();
-            var yPos = Objects[i].transform.position.y.ToString();
-            var newLine = string.Format("{0},{1},{2},{3}", PlayerID.id, Objects[i].name, xPos, yPos);
-            csv.AppendLine(newLine);
+            var xPos = Objects[i].transform.position.x;
+            var yPos = Objects[i].transform.position.y;
+            SendMapCoordinatesToServer(PlayerID.id, Objects[i].name, xPos, yPos);
         }
-        string filePath = @"UserData/MapData/" + PlayerID.id + ".csv";
-        //after your loop
-        File.WriteAllText(filePath, csv.ToString());
-        // move to next scene
+
+        // Move to next scene
         SceneManager.LoadScene("Finished");
+    }
+
+    public void SendMapCoordinatesToServer(string playerId, string storeName, float xPos, float yPos)
+    {
+        StartCoroutine(SendMapCoordinatesCoroutine(playerId, storeName, xPos, yPos));
+    }
+
+    private IEnumerator SendMapCoordinatesCoroutine(string playerId, string storeName, float xPos, float yPos)
+    {
+        var data = new
+        {
+            table_name = "mapcoordinates",
+            participant_id = playerId,
+            store_name = storeName,
+            x_pos = xPos,
+            y_pos = yPos
+        };
+
+        string jsonData = JsonUtility.ToJson(data);
+
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(apiUrl, ""))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
+            www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            www.downloadHandler = new DownloadHandlerBuffer();
+            www.SetRequestHeader("Content-Type", "application/json");
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Map coordinates data sent successfully");
+            }
+        }
     }
 }
